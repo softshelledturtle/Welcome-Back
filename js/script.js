5905efaf5ec73ea86963c9d13120165c366aebc1
Bearjiajia <zxcvbnm71327@gmail.com>
2022-12-25 16:53:07 +0800

    插圖前4個有彈出幻燈片

--- js/script.js
+++ js/script.js
@@ -18,39 +18,40 @@ function closeModal(id) {
     modal.style.display = "none"; // 隱藏
   }
   
-  
-// 幻燈片索引
 var slideIndex = 1;
 
 // 显示幻燈片函数
-function showSlides(n) {
-  // 幻燈片
-  var slides = document.getElementsByClassName("mySlides");
-  // 文字容器
-  var captions = document.getElementsByClassName("caption-container");
-  // 如果幻燈片索引大於最大索引，則设置为 1
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  // 如果幻燈片索引小於最小索引，則设置为最大索引
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  // 隐藏所有幻燈片
-  for (var i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  // 隐藏所有文字容器
-  for (var i = 0; i < captions.length; i++) {
-    captions[i].style.display = "none";
-  }
-  // 显示当前幻燈片
-  slides[slideIndex-1].style.display = "block";
-  // 显示当前文字容器
-  captions[slideIndex-1].style.display = "block";
+function showSlides(classname,n = 1) {
+    // 幻燈片
+    var slides = document.getElementsByClassName(classname);
+
+    // 文字容器
+    var captions = document.getElementsByClassName("caption-container");
+
+    // 如果幻燈片索引大於最大索引，則设置为 1
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    // 如果幻燈片索引小於最小索引，則设置为最大索引
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    // 隐藏所有幻燈片
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    // 隐藏所有文字容器
+    for (var i = 0; i < captions.length; i++) {
+        captions[i].style.display = "none";
+    }
+    // 显示当前幻燈片
+    slides[slideIndex-1].style.display = "block";
+    // 显示当前文字容器
+    captions[slideIndex-1].style.display = "block";
+
 }
 
 // 切換幻燈片函数
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(classname,n) {
+  showSlides(classname,slideIndex += n);
 }
